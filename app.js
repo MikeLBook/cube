962308625c80dfe1ee0fb4e7c399e05bc8a4913a
Michael Book <mikebook24@gmail.com>
2026-06-16 23:32:42 -0500

    debugging

--- app.js
+++ app.js
@@ -422,33 +422,39 @@
       init_helpers();
       init_RubiksCube();
       var rubiksCube = RubiksCube.getInstance();
-      document.querySelectorAll(".cube").forEach((el) => {
-        const cubeElement = el;
-        const orientation = cubeElement.dataset.orientation;
-        const position = parseInt(cubeElement.dataset.position);
-        const cube = rubiksCube.cubes.find((c) => {
-          return JSON.stringify(c.position) === JSON.stringify(positionMap[position]);
-        });
-        console.log("orientation", cube.orientation);
-        switch (orientation) {
-          case "top":
-            cubeElement.classList.add(cube.orientation.top);
-            break;
-          case "left":
-            cubeElement.classList.add(cube.orientation.left);
-            break;
-          case "front":
-            cubeElement.classList.add(cube.orientation.front);
-            break;
-          case "right":
-            cubeElement.classList.add(cube.orientation.right);
-            break;
-          case "bottom":
-            cubeElement.classList.add(cube.orientation.bottom);
-            break;
-        }
+      document.querySelector("#rotateTopCCW")?.addEventListener("click", () => {
+        rubiksCube.rotateTopCCW();
+        renderCube();
       });
-      console.log(rubiksCube);
+      function renderCube() {
+        console.log(rubiksCube);
+        document.querySelectorAll(".cube").forEach((el) => {
+          const cubeElement = el;
+          const orientation = cubeElement.dataset.orientation;
+          const position = parseInt(cubeElement.dataset.position);
+          const cube = rubiksCube.cubes.find((c) => {
+            return JSON.stringify(c.position) === JSON.stringify(positionMap[position]);
+          });
+          switch (orientation) {
+            case "top":
+              cubeElement.classList.add(cube.orientation.top);
+              break;
+            case "left":
+              cubeElement.classList.add(cube.orientation.left);
+              break;
+            case "front":
+              cubeElement.classList.add(cube.orientation.front);
+              break;
+            case "right":
+              cubeElement.classList.add(cube.orientation.right);
+              break;
+            case "bottom":
+              cubeElement.classList.add(cube.orientation.bottom);
+              break;
+          }
+        });
+      }
+      renderCube();
     }
   });
   require_app();
